cfbd2aa50b9d8804ff7abb0c182f1c54ef7a5b6a
rafael <rafael.cds.monteiro@hotmail.com>
2020-04-13 21:27:08 -0300

    Adding user DAO

--- app.py
+++ app.py
@@ -3,23 +3,28 @@ import mastermind_logic as l_master
 app = Flask(__name__)
 
 
-@app.route("/generate-number", methods=['GET'])
+@app.route("/generate-number/", methods=['GET'])
 def random_number():
     str_number = l_master.generate_number()
-    return str_number
+    return str_number, 200
 
 
-@app.route("/initialize", methods=['GET'])
+@app.route("/initialize/", methods=['GET'])
 def to_txt():
     resolution = l_master.to_txt()
-    return resolution
+    return resolution, 200
 
 
-@app.route("/mastermind/<string:number_typed>", methods=['GET'])
+@app.route("/mastermind/<string:number_typed>/", methods=['GET'])
 def mastermind(number_typed):
     to_send = l_master.master_mind(number_typed)
-    return to_send
+    return to_send, 200
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'A página não existe.', 404
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, threaded=True)
---
+++ user_dao.py
@@ -0,0 +1,49 @@
+import pymongo
+
+
+# Getting connection with mongodb
+def db_connection():
+    my_connection = pymongo.MongoClient("mongodb://localhost:27017/")
+    my_db = my_connection["mastermindDB"]
+    return my_db
+
+
+def creating_user(user_data):
+    connection = db_connection()
+    my_col = connection["users"]
+    x = my_col.insert_one(user_data)
+    print(x.inserted_id)
+
+
+# This function add a new line element on a specific document.
+def updating_user(user_key, dict_update):
+    connection = db_connection()
+    my_col = connection["users"]
+    user = find_user(user_key)
+    # Setting the new content.
+    new_values = {"$set": dict_update}
+    # Merging the content with the existent content.
+    my_col.update_one(user, new_values)
+
+    user = find_user(user_key)
+    print(user)
+
+
+def find_users():
+    connection = db_connection()
+    my_col = connection["users"]
+    for x in my_col.find():
+        print(x)
+
+
+def find_user(user_key):
+    connection = db_connection()
+    my_col = connection["users"]
+    my_query = {"name": user_key}
+    x = my_col.find_one(my_query, {"_id": 0})
+    return x
+
+
+if __name__ == '__main__':
+    update_dict = {"description": "rafael é uma pessoa confiavel."}
+    updating_user("Rafael", update_dict)
