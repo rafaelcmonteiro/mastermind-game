0536a00c448509455bf2a4cd74a30b5b192c7b60
rafael <rafael.cds.monteiro@hotmail.com>
2020-04-16 20:21:53 -0300

    Adding some treatments to inputs.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
 import mastermind_logic as l_master
 
-app = Flask("__name__")
-#app.config['SECRET_KEY'] = '57956B56B56545B'
+app = Flask("mastermind")
+app.config['SECRET_KEY'] = '57956B56B56545B'
 
 
 @app.route("/")
@@ -21,9 +21,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login/")
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='login', form=form)
 
 
