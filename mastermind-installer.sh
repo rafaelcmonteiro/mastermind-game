4707a03bbf4f24c523027979446dba80e208e050
rafael <rafael.cds.monteiro@hotmail.com>
2020-04-23 00:07:50 -0300

    settings

--- mastermind-installer.sh
+++ mastermind-installer.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+cd
 echo 'Downloading repository...'
 wget https://github.com/rafael31101995/Mastermind-Game/archive/master.zip
 echo 'unzip repository'
-unzip master.zip
+unzip ~/master.zip
 echo 'removing repository .zip'
-rm -r master.zip
+rm -r ~/master.zip
 echo 'trying to install pip virtualenv'
 pip install virtualenv
 echo 'creating venv'
-virtualenv -p python3 Mastermind-Game-master/venv
+virtualenv -p python3 ~/Mastermind-Game-master/venv
 echo 'activate venv'
-source Mastermind-Game-master/venv/bin/activate
+source ~/Mastermind-Game-master/venv/bin/activate
 echo 'installing requirements'
-pip install -r Mastermind-Game-master/requirements.txt
+pip install -r ~/Mastermind-Game-master/requirements.txt
+
